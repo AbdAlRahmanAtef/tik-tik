dceba1402391daabcecfc83cde4d52dfe0eb08d9
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2022-12-13 09:03:43 +0200

    modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -60,7 +60,7 @@ const Navbar = () => {
           <div className="flex gap-5 md:gap-10">
             <Link href="/upload">
               <button className="border-2 px-2 py-2 md:px-4 text-md font-semibold flex items-center gap-2">
-                <IoMdAdd className="text-xl" />{" "}
+                <IoMdAdd className="text-xl" />
                 <span className="hidden md:block">Upload </span>
               </button>
             </Link>
